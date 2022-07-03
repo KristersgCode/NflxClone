76da922c32abb360885e678727b7e62d6905a527
KristersgCode <kristersgutmanis@gmail.com>
2022-07-03 15:26:19 +0300

    changed styles

--- components/FaqElements.js
+++ components/FaqElements.js
@@ -9,14 +9,15 @@ function FaqElements({ answer, question }) {
   };
 
   return (
-    <div className="mt-2 w-full  sm:w-[80%] md:w-[80%] lg:w-[700px]">
+    <div className="mt-2 w-full  sm:w-[80%] md:w-[80%] lg:w-[800px]">
       <details className="details-color">
-        <summary className="details-color flex w-[100%] justify-between">
+        <summary className="details-color flex w-[100%]  justify-between">
           <div
             onClick={handleToggle}
             className="pl-5 flex w-full h-14 cursor-pointer items-center justify-between lg:h-[70px]"
           >
             <p className="font-light sm:text-xl lg:text-2xl">{question}</p>
+
             <PlusIcon className={isActive ? "icon-class" : "icon-class2"} />
           </div>
         </summary>
